9ec3cfbbe4ba1cd1d1d28950d8c5a65e247775d3
André Archanjo <archanjoandre@yahoo.com.br>
2019-06-29 15:08:28 -0300

    Criado o repositorio do usuario

---
+++ src/main/java/br/ufjf/dcc193/trb3/repository/UsuarioRepository.java
@@ -0,0 +1,15 @@
+package br.ufjf.dcc193.trb3.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.ufjf.dcc193.trb3.model.Usuario;
+
+/**
+ * UsuarioRepository
+ */
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    
+}
