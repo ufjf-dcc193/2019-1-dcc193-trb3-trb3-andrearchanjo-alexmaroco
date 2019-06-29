f1d7536740f61c382e6f1a35231ddfe267be1be8
André Archanjo <archanjoandre@yahoo.com.br>
2019-06-29 16:55:47 -0300

    Exclusão de usuário

--- src/main/java/br/ufjf/dcc193/trb3/controller/UsuarioController.java
+++ src/main/java/br/ufjf/dcc193/trb3/controller/UsuarioController.java
@@ -10,6 +10,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.ufjf.dcc193.trb3.model.Usuario;
@@ -57,6 +58,14 @@ public class UsuarioController {
         mv.addObject("usuarios", users);
         System.err.println(users.toString());
         mv.setViewName("list-usuarios");
+        return mv;
+    }
+
+    @GetMapping(value={"/excluir.html" })
+    public ModelAndView excluirUsuario(@RequestParam Long id) {
+        ModelAndView mv = new ModelAndView();
+        uRepo.deleteById(id);
+        mv.setViewName("redirect:/usuario/listar.html");
         return mv;
     }
     
