2c384bce76234c53acdf63105d3f467c2787b4ba
André Archanjo <archanjoandre@yahoo.com.br>
2019-06-29 14:45:13 -0300

    Adicionado constraints

--- src/main/java/br/ufjf/dcc193/trb3/model/Etiqueta.java
+++ src/main/java/br/ufjf/dcc193/trb3/model/Etiqueta.java
@@ -1,6 +1,10 @@
 package br.ufjf.dcc193.trb3.model;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 /**
  * Etiqueta
@@ -8,15 +12,58 @@ import javax.persistence.Entity;
 @Entity
 public class Etiqueta {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private Long id;
 
+    @NotBlank(message = "O campo não pode estar vazio")
     private String titulo;
 
+    @NotBlank(message = "O campo não pode estar vazio")
     private String descricao;
 
+    @NotBlank(message = "O campo não pode estar vazio")
     private String url;
 
     public Etiqueta() {}
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "Etiqueta [descricao=" + descricao + ", id=" + id + ", titulo=" + titulo + ", url=" + url + "]";
+    }
+
+    
     
 }
