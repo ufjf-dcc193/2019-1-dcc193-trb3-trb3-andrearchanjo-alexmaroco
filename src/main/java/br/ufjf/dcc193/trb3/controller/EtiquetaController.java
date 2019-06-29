55fa5051b0c03218f222ab7449935f63c74fce67
Alex Maroco <alexsmaroco@gmail.com>
2019-06-29 20:21:56 -0300

    Adiciona controlador para Etiqueta + CRUD

---
+++ src/main/java/br/ufjf/dcc193/trb3/controller/EtiquetaController.java
@@ -0,0 +1,89 @@
+package br.ufjf.dcc193.trb3.controller;
+
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.ufjf.dcc193.trb3.model.Etiqueta;
+import br.ufjf.dcc193.trb3.repository.EtiquetaRepository;
+
+/**
+ * EtiquetaController
+ */
+@Controller
+@RequestMapping("/etiqueta")
+public class EtiquetaController {
+
+    @Autowired
+    private EtiquetaRepository etRepo;
+
+    @GetMapping("/cadastro.html")
+    public ModelAndView cadastroUsuario() {
+        ModelAndView mv = new ModelAndView();
+        System.err.println(etRepo.findAll());
+        mv.addObject("etiqueta", new Etiqueta());
+        mv.setViewName("form-cadastro-etiqueta");
+        return mv;
+    }
+
+    @PostMapping("/cadastro.html")
+    public ModelAndView cadastroUsuario(@Valid Etiqueta etiqueta, BindingResult binding) {
+        ModelAndView mv = new ModelAndView();
+        if(binding.hasErrors()){
+            mv.setViewName("form-cadastro-etiqueta");
+            mv.addObject("etiqueta", etiqueta);
+            return mv;
+        }
+        etRepo.save(etiqueta);
+        mv.setViewName("redirect:/index.html");
+        return mv;
+    }
+
+    @GetMapping(value={"/listar.html"})
+    public ModelAndView listarTodos() {
+        ModelAndView mv = new ModelAndView();
+        List<Etiqueta> etiquetas = etRepo.findAll();
+        mv.addObject("etiquetas", etiquetas);
+        mv.setViewName("list-etiquetas");
+        return mv;
+    }
+
+    @GetMapping(value={"/excluir.html" })
+    public ModelAndView excluirUsuario(@RequestParam Long id) {
+        ModelAndView mv = new ModelAndView();
+        etRepo.deleteById(id);
+        mv.setViewName("redirect:/etiqueta/listar.html");
+        return mv;
+    }
+
+    @GetMapping(value={"/editar.html" })
+    public ModelAndView editarEtiqueta(@RequestParam Long id) {
+        ModelAndView mv = new ModelAndView();
+        Etiqueta etiqueta = etRepo.findById(id).get();
+        mv.addObject("etiqueta", etiqueta);
+        mv.setViewName("form-edit-etiqueta");
+        return mv;
+    }
+
+    @PostMapping(value={"/editar.html" })
+    public ModelAndView editarEtiqueta(@Valid Etiqueta etiqueta, BindingResult binding) {
+        ModelAndView mv = new ModelAndView();
+            if(binding.hasErrors()){
+                mv.setViewName("form-edit-etiqueta");
+                mv.addObject("etiqueta", etiqueta);
+                return mv;
+            }
+            etRepo.save(etiqueta);
+            mv.setViewName("redirect:/etiqueta/listar.html");
+            return mv;
+    }
+}
