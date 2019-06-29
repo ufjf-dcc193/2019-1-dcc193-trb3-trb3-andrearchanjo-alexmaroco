b768b03f450a2d28ef4e450d2a14ebbabbc46286
André Archanjo <archanjoandre@yahoo.com.br>
2019-06-29 14:44:52 -0300

    Adicionado constraints e mapeamento

--- src/main/java/br/ufjf/dcc193/trb3/model/Vinculo.java
+++ src/main/java/br/ufjf/dcc193/trb3/model/Vinculo.java
@@ -2,7 +2,12 @@ package br.ufjf.dcc193.trb3.model;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 /**
  * Vinculo
@@ -10,6 +15,8 @@ import javax.persistence.Entity;
 @Entity
 public class Vinculo {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.TABLE)
     private Long id;
 
     // Nao fazer manytoone com item(deixar unilateral), pq nao precisa fazer nada com itens de um vinculo
@@ -17,9 +24,62 @@ public class Vinculo {
 
     private Long idItemDestino;
 
-    private List<Anotacao> anotacoes;
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Anotacao> vinculo_anotacoes;
 
-    private List<Etiqueta> etiquetas;
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Etiqueta> vinculo_etiquetas;
 
     public Vinculo() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getIdItemOrigem() {
+        return idItemOrigem;
+    }
+
+    public void setIdItemOrigem(Long idItemOrigem) {
+        this.idItemOrigem = idItemOrigem;
+    }
+
+    public Long getIdItemDestino() {
+        return idItemDestino;
+    }
+
+    public void setIdItemDestino(Long idItemDestino) {
+        this.idItemDestino = idItemDestino;
+    }
+
+    public List<Anotacao> getVinculo_anotacoes() {
+        return vinculo_anotacoes;
+    }
+
+    public void setVinculo_anotacoes(List<Anotacao> vinculo_anotacoes) {
+        this.vinculo_anotacoes = vinculo_anotacoes;
+    }
+
+    public List<Etiqueta> getVinculo_etiquetas() {
+        return vinculo_etiquetas;
+    }
+
+    public void setVinculo_etiquetas(List<Etiqueta> vinculo_etiquetas) {
+        this.vinculo_etiquetas = vinculo_etiquetas;
+    }
+
+    @Override
+    public String toString() {
+        return "Vinculo [anotacoes=" + vinculo_anotacoes + ", etiquetas=" + vinculo_etiquetas + ", id=" + id + ", idItemDestino="
+                + idItemDestino + ", idItemOrigem=" + idItemOrigem + "]";
+    }
+
+    
+
+    
+
 }
