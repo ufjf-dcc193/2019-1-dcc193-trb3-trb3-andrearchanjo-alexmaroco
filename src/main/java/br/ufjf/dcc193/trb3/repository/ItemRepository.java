f9afd8ec8a58e3e4e59021e133bbd37b0e641986
André Archanjo <archanjoandre@yahoo.com.br>
2019-06-30 21:22:23 -0300

    Criação de uma custom query

--- src/main/java/br/ufjf/dcc193/trb3/repository/ItemRepository.java
+++ src/main/java/br/ufjf/dcc193/trb3/repository/ItemRepository.java
@@ -1,6 +1,10 @@
 package br.ufjf.dcc193.trb3.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import br.ufjf.dcc193.trb3.model.Item;
@@ -11,5 +15,7 @@ import br.ufjf.dcc193.trb3.model.Item;
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long>{
 
-    
+    @Query(value = "select * from itens i where i.id != :id", nativeQuery = true)
+    List<Item> getAlmostAllItems(@Param("id") Long id);
+
 }
