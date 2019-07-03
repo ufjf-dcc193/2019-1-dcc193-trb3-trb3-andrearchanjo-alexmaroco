c1a2666a55aa243e07812d6f090463757209a3c5
André Archanjo <archanjoandre@yahoo.com.br>
2019-07-02 22:02:55 -0300

    CRUD do cadastrar anotacao em um vinculo

--- src/main/java/br/ufjf/dcc193/trb3/controller/AnotacaoController.java
+++ src/main/java/br/ufjf/dcc193/trb3/controller/AnotacaoController.java
@@ -17,6 +17,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import br.ufjf.dcc193.trb3.model.Anotacao;
 import br.ufjf.dcc193.trb3.model.Item;
+import br.ufjf.dcc193.trb3.model.Vinculo;
 import br.ufjf.dcc193.trb3.repository.AnotacaoRepository;
 import br.ufjf.dcc193.trb3.repository.ItemRepository;
 import br.ufjf.dcc193.trb3.repository.VinculoRepository;
@@ -40,6 +41,8 @@ public class AnotacaoController {
 
     @Autowired
     private VinculoRepository vRepo;
+
+    /////////////////////Cadastro de anotação no item
 
     @GetMapping("/cadastro.html")
     public ModelAndView cadastroAnotacao(@RequestParam(required = false) Long idItem) {
@@ -136,6 +139,112 @@ public class AnotacaoController {
         aRepo.save(anotacao);
         //System.err.println(v);
         mv.setViewName("redirect:/anotacao/listar.html?idItem="+idItem);
+        return mv;
+    }
+
+    /////////////////////Cadastro de anotação no vínculo
+
+    @GetMapping("/cadastro2.html")
+    public ModelAndView cadastroAnotacao2(@RequestParam(required = false) Long idItem, @RequestParam Long idVinculo) {
+        ModelAndView mv = new ModelAndView();
+        // System.err.println(iRepo.findAll());
+        
+        if(ls.getUser() == null){
+            mv.setViewName("redirect:/vinculo/listar.html?id="+idItem);
+            return mv;
+        }
+
+        if(idItem != null){
+            System.err.println("ID:" + idItem);
+            Item i = iRepo.findById(idItem).get();
+            mv.addObject("item", i);
+        }
+
+        Vinculo vinculo = vRepo.findById(idVinculo).get();
+        mv.addObject("vinculo", vinculo);
+        mv.addObject("anotacao", new Anotacao());
+        mv.setViewName("form-cadastro-anotacao2");
+        return mv;
+    }
+
+    @PostMapping("/cadastro2.html")
+    public ModelAndView cadastroAnotacao2(@Valid Anotacao anotacao, @RequestParam(required = false) Long idVinculo, @RequestParam Long idItem, BindingResult binding) {
+        ModelAndView mv = new ModelAndView();
+        if(binding.hasErrors()){
+            mv.setViewName("form-cadastro-anotacao2");
+            mv.addObject("idVinculo", idVinculo);
+            mv.addObject("anotacao", anotacao);
+            return mv;
+        }
+        anotacao.setIdCriador(ls.getUser().getId());
+        anotacao.setDataInclusao(new Date());;
+        if(idVinculo != null){
+            Vinculo v = vRepo.findById(idVinculo).get();
+            v.getVinculo_anotacoes().add(anotacao);
+        }
+        aRepo.save(anotacao);
+        // System.err.println(item);
+        mv.setViewName("redirect:/vinculo/listar.html?id="+idItem);
+        return mv;
+    }
+
+    @GetMapping(value={"/listar2.html"})
+    public ModelAndView listarTodos2(@RequestParam Long idItem, @RequestParam Long idVinculo) {
+        ModelAndView mv = new ModelAndView();
+        Vinculo v = vRepo.findById(idVinculo).get();
+        List<Anotacao> anotacoes = v.getVinculo_anotacoes();
+        mv.addObject("anotacoes", anotacoes);
+        mv.addObject("idItem", idItem);
+        mv.addObject("idVinculo", v.getId());
+        mv.setViewName("list-anotacoes2");
+        return mv;
+    }
+
+    @GetMapping(value={"/excluir2.html" })
+    public ModelAndView excluirAnotacao2(@RequestParam Long id, @RequestParam Long idItem, @RequestParam Long idVinculo) {
+        ModelAndView mv = new ModelAndView();
+        Anotacao a = aRepo.findById(id).get();
+        Vinculo v = vRepo.findById(idVinculo).get();
+        
+        v.getVinculo_anotacoes().remove(a);
+        aRepo.delete(a);
+        
+        mv.setViewName("redirect:/anotacao/listar2.html?idItem="+idItem+"&idVinculo="+idVinculo);
+        return mv;
+    }
+
+    @GetMapping(value={"/editar2.html" })
+    public ModelAndView editarAnotacao2(@RequestParam Long id, @RequestParam Long idItem, @RequestParam Long idVinculo) {
+        ModelAndView mv = new ModelAndView();
+        Anotacao anotacao = aRepo.findById(id).get();
+        mv.addObject("idVinculo", idVinculo);
+        mv.addObject("idItem", idItem);
+        mv.addObject("anotacao", anotacao);
+        mv.setViewName("form-edit-anotacao2");
+        return mv;
+    }
+
+    @PostMapping(value={"/editar2.html" })
+    public ModelAndView editarAnotacao2(@RequestParam Long id, @Valid Anotacao anotacao, @RequestParam Long idItem, @RequestParam Long idVinculo, BindingResult binding) {
+        ModelAndView mv = new ModelAndView();
+        if(binding.hasErrors()){
+            mv.setViewName("form-edit-anotacao2");
+            mv.addObject("idVinculo", idVinculo);
+            mv.addObject("idItem", idItem);
+            mv.addObject("anotacao", anotacao);
+            return mv;
+        }
+        Anotacao a = aRepo.findById(id).get();
+        anotacao.setIdCriador(a.getIdCriador());
+        anotacao.setDataInclusao(a.getDataInclusao());
+        anotacao.setDataAlteracao(new Date());
+        // Remove a referencia aos antigos vinculos
+        
+        String[] ignorar = {"id"};
+        BeanUtils.copyProperties(anotacao, a, ignorar);
+        aRepo.save(anotacao);
+        //System.err.println(v);
+        mv.setViewName("redirect:/anotacao/listar2.html?idItem="+idItem+"&idVinculo="+idVinculo);
         return mv;
     }
 
